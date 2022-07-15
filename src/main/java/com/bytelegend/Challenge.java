b3b21bd3ec0737166c2e056fde2b70fb2ec05952
Birlinha <omenaribeiro13@gmail.com>
2022-07-15 23:03:03 +0000

    Update at 2022-07-15T23:03:03.587439Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -4,7 +4,7 @@ public class Challenge {
     public static void main(String[] args) {
         System.out.println(countUpperCaseLetters("AaBbCc1234ABC"));
     }
-
+    
     /**
      * `countUpperCaseLetters()` method returns the number of upper case letters in the given
      * English string.
@@ -12,6 +12,15 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        Object[] stringList = s.split("");
+
+        Int i = 0;
+        for(int i = 0; i < stringList.length ; i++0) {
+            if(stringList[i].isUpperCase) {
+                i += 1;
+            }
+        }
+
+        return i;
     }
 }
