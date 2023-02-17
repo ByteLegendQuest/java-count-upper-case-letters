3b16e37c88133ceaaf2dfb4474d030a13af999af
Ajieoooo <1823451579@qq.com>
2023-02-17 01:15:24 +0000

    Update at 2023-02-17T01:15:24.393571Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,6 +12,14 @@ public class Challenge {
      * <p>For example, `countUpperCaseLetters("1A2B3C4d")` returns `3`.
      */
     public static int countUpperCaseLetters(String s) {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (char j = 'A'; j < 'Z'; j++) {
+                if (s.charAt(i) == j) {
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 }
